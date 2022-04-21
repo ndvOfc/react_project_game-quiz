6898e9df15c6df588968ad056de02bb73f6dd8ac
Den <ndvofc@gmail.com>
2022-04-21 23:16:58 +0300

    server done

--- server/routes/stateRoutes.js
+++ server/routes/stateRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const stateUserController = require('../controllers/stateController')
 
-router.get('/', stateUserController.getOneUserState)
+router.get('/', stateUserController.getAllUsersState)
 
-router.get('/:id', stateUserController.getAllUsersState )
+router.get('/:id', stateUserController.getOneUserState )
 
 module.exports = router;

563d7106f5a7c9cd028b48eb1aca59f6d9d0842d
Petr <ptrklsnkv@gmail.com>
2022-04-21 16:58:10 +0300

    Create skelton for all team

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import{Provider} from 'react-redux'
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+
+    </div>
+  );
+}
+
+export default App;

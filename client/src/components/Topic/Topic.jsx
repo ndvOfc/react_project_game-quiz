e81496245a0ac04d63786ca40e02719ff4192e3e
Petr <ptrklsnkv@gmail.com>
2022-04-21 17:38:40 +0300

    create brow router and provider

---
+++ client/src/components/Topic/Topic.jsx
@@ -0,0 +1,34 @@
+
+const Topic = () => {
+
+  return (
+    <div className="container">
+      <div className="row row-cols-auto">
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+      </div>
+      <div className="row row-cols-auto">
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+      </div>
+      <div className="row row-cols-auto">
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+      </div>
+    </div>
+  )
+}
+
+export default Topic;

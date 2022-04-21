bd7a67888c3b726a544add1130060c6dabf66d45
Petr <ptrklsnkv@gmail.com>
2022-04-21 20:27:15 +0300

    merge branches

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import topicsReducer from "./topicsReducer";
 
 export const rootReducer = combineReducers({
-
+  topics: topicsReducer
 })
---
+++ client/src/redux/reducers/topicsReducer.js
@@ -0,0 +1,13 @@
+
+// const initialState = { topics: [], questions: [], sessionScore: 0}
+
+// const topicsReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case CHANGE_SCORE:
+//       let copyTopicsQuestionScore = {...state}
+//       copyTopicsQuestionScore.sessionScore += state.sessionScore
+//       return { ...state}
+//   }
+// }
+
+// export default topicsReducer;

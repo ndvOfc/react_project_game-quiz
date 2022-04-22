50464497d90aa61a9256e90b54139ee968654d55
Den <ndvofc@gmail.com>
2022-04-22 12:25:46 +0300

    stage4

--- server/controllers/groupController.js
+++ server/controllers/groupController.js
@@ -5,12 +5,19 @@ class GroupController {
     async getGroup(req, res) {
         const { id } = req.params
         const array = []
+        const questionsArray = []
 
-        for
-        const groupToTopic = await Topic.findAll({where: {groupId: id}, raw: true})
+        const topicNames =  await Topic.findAll({where: {groupId:id }, raw:true})
 
-        const questions = await Questions.findAll({where : {topicId : groupToTopic[1].id}})
-        res.json({topics: groupToTopic, questions })
+        for (let i = 0; i < topicNames.length; i ++) {
+            array.push(
+                { topic: topicNames[i].topicName,
+                  questions: await Questions.findAll({where : {topicId : topicNames[i].id}, raw:true})
+                }
+            )
+        }
+
+        res.json(array)
     }
 
     async getAllGroup(req, res) {
